55d0fc715550899404581d39c5c76b9364062a52
odinokov <dimka77783@mail.ru>
2024-01-30 20:53:50 +0300

    3/05

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,25 +1,20 @@
 from django.shortcuts import render
 
-#from .forms import FindForms
+from .forms import FindForms
 from .models import Vacancy
 
 
 def home_view(request):
-    qs = Vacancy.objects.all()
-    return render(request, 'home.html', {'object_list': qs })
-    #form = FindForms()
-    #city = request.GET.get('city')
-    #language = request.GET.get('language')
-    #qs = []
-    #if city or language:
-     #   _filter = {}
-      #  if city:
-       #     _filter['city__slug'] = city
-        #if language:
-        #    _filter['language__slug'] = language
+    form = FindForms()
+    city = request.GET.get('city')
+    language = request.GET.get('language')
+    qs = []
+    if city or language:
+        _filter = {}
+        if city:
+            _filter['city__slug'] = city
+        if language:
+            _filter['language__slug'] = language
 
-        #qs = Vacancy.objects.filter(**_filter)
-    #return render(request, 'scraping/base.html', {'object_list': qs, 'form': form})
-#from django.shortcuts import render
-
-# Create your views here.
+        qs = Vacancy.objects.filter(**_filter)
+    return render(request, 'scraping/home.html', {'object_list': qs, 'form': form})
