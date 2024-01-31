b5494936f2f1970dc6c60f61afb51269c0808e3a
odinokov <dimka77783@mail.ru>
2024-01-31 21:43:40 +0300

    5/11

--- src/scraping_service/urls.py
+++ src/scraping_service/urls.py
@@ -1,4 +1,4 @@
-"""scraping_cervice URL Configuration
+"""scraping_service URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
--- src/scraping_service/wsgi.py
+++ src/scraping_service/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for scraping_cervice project.
+WSGI config for scraping_service project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scraping_cervice.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scraping_service.settings')
 
 application = get_wsgi_application()
