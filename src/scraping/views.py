d3ab9eeafaab723ef9bb283387e9886537759a4d
odinokov <dimka77783@mail.ru>
2024-02-05 21:25:28 +0300

    7/07

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from .forms import FindForms
@@ -7,8 +8,15 @@ from .models import Vacancy
 def home_view(request):
     form = FindForms()
     city = request.GET.get('city')
+
+    return render(request, 'scraping/home.html', {'form': form})
+
+
+def list_view(request):
+    form = FindForms()
+    city = request.GET.get('city')
     language = request.GET.get('language')
-    qs = []
+    context = {'city': city, 'language': language, 'form': form}
     if city or language:
         _filter = {}
         if city:
@@ -17,4 +25,10 @@ def home_view(request):
             _filter['language__slug'] = language
 
         qs = Vacancy.objects.filter(**_filter)
-    return render(request, 'scraping/home.html', {'object_list': qs, 'form': form})
+        paginator = Paginator(qs, 10)  # Show 25 contacts per page.
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context['object_list'] = page_obj
+
+    return render(request, 'scraping/list.html',context)
