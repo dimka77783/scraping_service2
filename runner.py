b90d8283863c192a43319e8746330c931b94a9df
dimka77783 <91821862+dimka77783@users.noreply.github.com>
2024-02-16 16:42:18 +0300

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,12 +1,6 @@
-
-import schedule
 import time
- 
-def job():
-    exec(open('run.py').read()) 
 
-schedule.every(1).minutes.do(job) 
- 
 while True:
-    schedule.run_pending()
-    time.sleep(2) 
+    exec(open('send.py').read())
+    
+    time.sleep(10)
